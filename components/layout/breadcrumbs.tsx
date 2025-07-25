4bf33739b50c46acfd50d3283925efc9c752e695
Jude Boachie <jdboachie@gmail.com>
2025-07-25 01:05:59 +0000

    edit breadcrumb skeleton

--- components/layout/breadcrumbs.tsx
+++ components/layout/breadcrumbs.tsx
@@ -35,7 +35,7 @@ const Breadcrumbs = () => {
     ? folder?.name || (
         <div className="flex flex-col gap-2.5">
           <Skeleton className="w-52 h-9" />
-          <Skeleton className="w-36 h-3" />
+          <Skeleton className="w-36 h-3.5" />
         </div>
       )
     : capitalize(segments[2] ?? "Home")
