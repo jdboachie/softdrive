b45676afebbda26d656d08b7b17f6660851b7074
Jude Boachie <jdboachie@gmail.com>
2025-07-28 22:03:37 +0000

    refactor: simplify UserImage component by removing Avatar wrapper and enhancing skeleton loading state

--- components/user-image.tsx
+++ components/user-image.tsx
@@ -1,29 +1,17 @@
 import Image from "next/image"
 import { Skeleton } from "./ui/skeleton"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { UserIcon } from "@phosphor-icons/react"
 
-export default function UserImage({ src }: { src: string | undefined }) {
+export default function UserImage ({ src }: { src: string | undefined }) {
+  if (src === undefined) {
+    return <Skeleton className={`rounded-full`} />
+  }
   return (
-    <>
-      {src ? (
-        <Avatar>
-          <AvatarImage asChild>
-            <Image
-              alt="recommender image"
-              src={src}
-              width={500}
-              height={500}
-              className="rounded-full"
-            />
-          </AvatarImage>
-          <AvatarFallback>
-            <UserIcon />
-          </AvatarFallback>
-        </Avatar>
-      ) : (
-        <Skeleton className="w-full h-full rounded-full" />
-      )}
-    </>
+    <Image
+      alt="recommender image"
+      src={src}
+      className={`rounded-full`}
+      width={"500"}
+      height={"500"}
+    />
   )
 }
