230e6489048b014ab0f692c7378b0406255582b3
Jude Boachie <jdboachie@gmail.com>
2025-07-28 22:07:46 +0000

    edit

--- components/file-search.tsx
+++ components/file-search.tsx
@@ -88,7 +88,7 @@ export default function FileSearch({
               className="group hover:bg-accent data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground [&_svg:not([class*='text-'])]:text-muted-foreground relative flex cursor-default items-center gap-2 rounded-sm p-2 py-2.5 text-sm outline-hidden select-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
             >
               <div className="flex items-center justify-between w-full gap-2">
-                <span className="flex items-center gap-2 truncate max-w-[70%]">
+                <span className="flex items-center gap-2 truncate max-w-[85%]">
                   <FileIcon type={file.isFolder ? "folder" : file.type} />
                   <span className="truncate">{file.name}</span>
                 </span>
